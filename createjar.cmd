dfa6198f82b8b2c3033f996a4c4377ca6a16d463
bjoggi5 <baj24@hi.is>
2023-02-10 10:15:52 +0000

    FINISHED!

---
+++ createjar.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files\Maven\apache-maven-3.9.0\bin\mvn" package
+"C:\Program Files\Maven\apache-maven-3.9.0\bin\mvn" install dependency:copy-dependencies
---
+++ runjar.cmd
@@ -0,0 +1,1 @@
+java -cp target/dependency/commons-numbers-primes-1.1.jar;target/ass4-1.0-SNAPSHOT.jar is.hi.baj24.hbv202g.App
